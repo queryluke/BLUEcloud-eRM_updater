7fc401e98d576070d1d39823af44bfc35300f128
Luke A <lukeaeschleman@gmail.com>
2018-07-17 14:58:00 -0400

    update un/pw path

--- config.php
+++ config.php
@@ -5,5 +5,4 @@ ini_set('display_errors', 1);
 $coral_path = '/software/coral';
 $coral_docs_url = "http://docs.coral-erm.org/";
 $sirsi_docs_url = "https://js-erm-helps.bc.sirsidynix.net/";
-$update_user = '';
-$update_pass = '';
+require_once '/root/.coral_upgrd_info'
