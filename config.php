a5d1772cf0af88995ee3e405b38aeccc834cb6ef
Luke A <lukeaeschleman@gmail.com>
2018-07-17 15:10:02 -0400

    Update config.php

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ ini_set('display_errors', 1);
 $coral_path = '/software/coral';
 $coral_docs_url = "http://docs.coral-erm.org/";
 $sirsi_docs_url = "https://js-erm-helps.bc.sirsidynix.net/";
-require_once '/root/.coral_upgrd_info'
+require_once '/root/.coral_upgrd_info';
