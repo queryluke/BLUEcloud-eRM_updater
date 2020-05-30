68504a264dd1759ee6302f9fa9e9d99349c70574
Luke Aeschleman <lukeaeschleman@gmail.com>
2020-05-30 16:51:26 -0400

    update 3.0.2 updater

--- 3.0.1-2.0.php
+++ 3.0.1-2.0.php
@@ -22,18 +22,9 @@ exec('git pull origin master');
 echo "Coral source code updated\n";
 
 
-foreach($modules as $m) {
-    $mod_name = ucfirst($m);
-    echo "Starting $mod_name updates\n";
-    if ($config[$m]['installed'] !== 'Y') {
-        continue;
-    }
-
-    if ($m == 'usage') {
-        process_sql_file($m, '3.0.2', '001-000.sql');
-    }
-    echo "$mod_name updates complete\n";
-}
+$sql_file_to_process = "$coral_path/usage/install/protected/3.0.2/001-000.sql";
+$command = "mysql -u{$update_user} -p{$update_pass} -h 127.0.0.1 -D coral_usage_prod < {$sql_file_to_process}";
+shell_exec($command);
 
 // update version in footer
 update_footer_version('3.0.1 (SirsiDynix 1.0)', '3.0.1 (SirsiDynix 2.0)');
